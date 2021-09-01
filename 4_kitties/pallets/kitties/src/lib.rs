a880f96ac717ca339c08bd877b4cbf1d9f57b7da
Han Zhao <windzhaohan@gmail.com>
2021-09-01 23:16:00 +0200

    Refactored code and added missing functionalities

--- 4_kitties/pallets/kitties/src/lib.rs
+++ 4_kitties/pallets/kitties/src/lib.rs
@@ -4,11 +4,11 @@ pub use pallet::*;
 #[frame_support::pallet]
 pub mod pallet {
 
-	use codec::{Codec, Decode, Encode};
+use codec::{Codec, Decode, Encode};
 	use frame_support::{dispatch::DispatchResult, pallet_prelude::*, traits::{Currency, ExistenceRequirement, Randomness, ReservableCurrency}};
 	use frame_system::pallet_prelude::*;
 	use sp_io::hashing::blake2_128;
-	use sp_runtime::traits::{AtLeast32BitUnsigned, Bounded, Saturating};
+	use sp_runtime::traits::{AtLeast32BitUnsigned, Bounded, Saturating, StaticLookup};
 
 	type BalanceOf<T> =
 	<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
@@ -76,6 +76,7 @@ pub mod pallet {
 		KittyAlreadyOwned,
 KittyWithoutOwner,
 KittyNotForSale,
+InsufficientReserveBalance,
 	}
 
 	#[pallet::call]
@@ -88,14 +89,7 @@ KittyNotForSale,
 
 			let dna = Self::random_value(&who);
 
-			Kitties::<T>::insert(kitty_id, Some(Kitty{dna: dna, list_price: None}));
-			Owner::<T>::insert(kitty_id, Some(who.clone()));
-			KittiesCount::<T>::put(kitty_id.saturating_add(T::KittyIndex::from(1u8)));
-
-			// Reserve amount of token
-			let _ = T::Currency::reserve(&who, T::ReserveAmount::get());
-
-			Self::deposit_event(Event::KittyCreated(who, kitty_id));
+			Self::kitty_create(who, kitty_id, dna)?;
 
 			Ok(())
 		}
@@ -103,16 +97,13 @@ KittyNotForSale,
 		#[pallet::weight(0)]
 		pub fn transfer(
 			origin: OriginFor<T>,
-			recipient: T::AccountId,
+			recipient: <T::Lookup as StaticLookup>::Source,
 			kitty_id: T::KittyIndex,
 		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
+			let recipient = T::Lookup::lookup(recipient)?;
 
-			ensure!(Some(who.clone()) == Owner::<T>::get(kitty_id), Error::<T>::NotOwner);
-
-			Owner::<T>::insert(kitty_id, Some(recipient.clone()));
-
-			Self::deposit_event(Event::KittyTransferred(who, recipient, kitty_id));
+			Self::kitty_transfer(who.clone(), recipient.clone(), kitty_id)?;
 
 			Ok(())
 		}
@@ -139,11 +130,7 @@ KittyNotForSale,
 				new_dna[i] = (selector[i] & kitty_1.dna[i]) | (!selector[i] & kitty_2.dna[i]);
 			}
 
-			Kitties::<T>::insert(kitty_id, Some(Kitty{dna: new_dna, list_price: None}));
-			Owner::<T>::insert(kitty_id, Some(who.clone()));
-			KittiesCount::<T>::put(kitty_id.saturating_add(T::KittyIndex::from(1u8)));
-
-			Self::deposit_event(Event::KittyCreated(who, kitty_id));
+			Self::kitty_create(who, kitty_id, new_dna)?;
 
 			Ok(())
 		}
@@ -193,7 +180,7 @@ KittyNotForSale,
 
 			let _ = T::Currency::transfer(&who, &original_owner, kitty_price, ExistenceRequirement::AllowDeath);
 
-			//TODO transfer kitty with a common method
+			Self::kitty_transfer(original_owner, who, kitty_id)?;
 
 			Ok(())
 		}
@@ -209,14 +196,54 @@ KittyNotForSale,
 			payload.using_encoded(blake2_128)
 		}
 
+		// Calculate the next kitty index
 		fn next_kitty_id() -> Result<T::KittyIndex, Error<T>> {
 			match Self::kitties_count() {
 				Some(id) => {
 					ensure!(id <= T::KittyIndex::max_value(), Error::<T>::KittiesCountOverflow);
 					Ok(id)
 				}
-				None => Ok(T::KittyIndex::min_value()),
+				None => Ok(T::KittyIndex::min_value().saturating_add(T::KittyIndex::from(1u8)))
 			}
 		}
+
+		// Create kitty
+		fn kitty_create(who: T::AccountId, kitty_id: T::KittyIndex, dna:[u8; 16]) -> Result<(), Error<T>> {
+
+			// Reserve amount of token
+			T::Currency::reserve(&who, T::ReserveAmount::get()).map_err(|_| Error::<T>::InsufficientReserveBalance)?;
+
+			Kitties::<T>::insert(kitty_id, Some(Kitty{dna: dna, list_price: None}));
+			Owner::<T>::insert(kitty_id, Some(who.clone()));
+			KittiesCount::<T>::put(kitty_id.saturating_add(T::KittyIndex::from(1u8)));
+
+			Self::deposit_event(Event::KittyCreated(who, kitty_id));
+
+			Ok(())
+		}
+
+		// Transfer kitty ownership and reserved tokens
+		fn kitty_transfer(
+			sender: T::AccountId,
+			recipient: T::AccountId,
+			kitty_id: T::KittyIndex
+		) -> Result<(), Error<T>> {
+
+			// Ensure the given kitty belongs to the sender
+			ensure!(Some(sender.clone()) == Owner::<T>::get(kitty_id), Error::<T>::NotOwner);
+
+			// Reserve amount of token on recipient
+			T::Currency::reserve(&recipient, T::ReserveAmount::get()).map_err(|_| Error::<T>::InsufficientReserveBalance)?;
+
+			Owner::<T>::insert(kitty_id, Some(recipient.clone()));
+
+			// Unreserve balance of sender
+			T::Currency::unreserve(&sender, T::ReserveAmount::get());
+
+			Self::deposit_event(Event::KittyTransferred(sender, recipient, kitty_id));
+
+			Ok(())
+		}
+
 	}
 }
