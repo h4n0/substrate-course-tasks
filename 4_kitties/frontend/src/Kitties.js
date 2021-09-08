9cac9f95b4f025b3f0057f77c7ab431572f602aa
Han Zhao <windzhaohan@gmail.com>
2021-09-08 22:37:36 +0200

    Added fetch and pop kitties functions

--- 4_kitties/frontend/src/Kitties.js
+++ 4_kitties/frontend/src/Kitties.js
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { Form, Grid } from 'semantic-ui-react'
+import React, { useEffect, useState } from "react";
+import { Form, Grid } from "semantic-ui-react";
 
-import { useSubstrate } from './substrate-lib'
-import { TxButton } from './substrate-lib/components'
+import { useSubstrate } from "./substrate-lib"
+import { TxButton } from "./substrate-lib/components";
 
-import KittyCards from './KittyCards'
+import KittyCards from "./KittyCards"
 
-export default function Kitties (props) {
+export default function Kitties(props) {
   const { api, keyring } = useSubstrate()
   const { accountPair } = props
 
-  const [kitties, setKitties] = useState([])
-  const [status, setStatus] = useState('')
+  const [kitties, setKitties] = useState([]);
+  const [status, setStatus] = useState('');
+
+  const [newKitty, setNewKitty] = useState(null);
+  const [kittiesCount, setKittiesCount] = useState(0);
+  const [kittiesDna, setKittiesDna] = useState([]);
+  const [kittiesOwner, setKittiesOwner] = useState([]);
 
   const fetchKitties = () => {
     // TODO: 在这里调用 `api.query.kittiesModule.*` 函数去取得猫咪的信息。
@@ -19,41 +24,85 @@ export default function Kitties (props) {
     //   - 共有多少只猫咪
     //   - 每只猫咪的主人是谁
     //   - 每只猫咪的 DNA 是什么，用来组合出它的形态
-  }
+    api.query.kitties.kittiesCount((c) => {
+      let count = 0;
+
+      if (c.isNone) {
+        return;
+      } else {
+        count = c.unwrap().toNumber();
+      }
+
+      let startIndex = 0;
+      if (kitties.length < count - 1) {
+        // Previous kitties missing, add them
+        startIndex = 1;
+      } else if (kitties.length > count - 1) {
+        // Rarely happen, maybe blockchain restarted, then reset kitties
+        setKitties([]);
+        startIndex = 1;
+      } else {
+		  // The newly added kitty is still not in kitties, therefore add this only
+		  startIndex = count;
+	  }
+
+      for (let i = startIndex; i <= count; i++) {
+        api.queryMulti(
+          [
+            [api.query.kitties.kitties, i],
+            [api.query.kitties.owner, i],
+          ],
+          ([kittyRaw, owner]) => {
+            const kitty = {
+              id: i,
+              dna: kittyRaw.unwrapOr(null).toU8a(),
+              owner: owner.unwrapOr(null).toHuman(),
+            };
+
+            setNewKitty(kitty);
+            console.log(kitty);
+          }
+        );
+      }
+    });
+  };
 
   const populateKitties = () => {
-    // TODO: 在这里添加额外的逻辑。你需要组成这样的数组结构：
-    //  ```javascript
-    //  const kitties = [{
-    //    id: 0,
-    //    dna: ...,
-    //    owner: ...
-    //  }, { id: ..., dna: ..., owner: ... }]
-    //  ```
-    // 这个 kitties 会传入 <KittyCards/> 然后对每只猫咪进行处理
-    const kitties = []
-    setKitties(kitties)
-  }
+    if (newKitty && newKitty.id > kitties.length) {
+      kitties.push(newKitty);
+      setKitties(kitties);
+            console.log(kitties);
+    }
+  };
 
-  useEffect(fetchKitties, [api, keyring])
-  useEffect(populateKitties, [])
+  useEffect(fetchKitties, [api, keyring]);
+  useEffect(populateKitties, [newKitty]);
 
-  return <Grid.Column width={16}>
-    <h1>小毛孩</h1>
-    <KittyCards kitties={kitties} accountPair={accountPair} setStatus={setStatus}/>
-    <Form style={{ margin: '1em 0' }}>
-      <Form.Field style={{ textAlign: 'center' }}>
-        <TxButton
-          accountPair={accountPair} label='创建小毛孩' type='SIGNED-TX' setStatus={setStatus}
-          attrs={{
-            palletRpc: 'kittiesModule',
-            callable: 'create',
-            inputParams: [],
-            paramFields: []
-          }}
-        />
-      </Form.Field>
-    </Form>
-    <div style={{ overflowWrap: 'break-word' }}>{status}</div>
-  </Grid.Column>
+  return (
+    <Grid.Column width={16}>
+      <h1>小毛孩</h1>
+      <KittyCards
+        kitties={kitties}
+        accountPair={accountPair}
+        setStatus={setStatus}
+      />
+      <Form style={{ margin: "1em 0" }}>
+        <Form.Field style={{ textAlign: "center" }}>
+          <TxButton
+            accountPair={accountPair}
+            label="创建小毛孩"
+            type="SIGNED-TX"
+            setStatus={setStatus}
+            attrs={{
+              palletRpc: "kitties",
+              callable: "create",
+              inputParams: [],
+              paramFields: [],
+            }}
+          />
+        </Form.Field>
+      </Form>
+      <div style={{ overflowWrap: "break-word" }}>{status}</div>
+    </Grid.Column>
+  );
 }
